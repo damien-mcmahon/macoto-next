97a61a02f851e5e92efe5b1f8daf261140b7f5c4
Damien McMahon <damien@macoto.co.uk>
2020-10-14 12:16:09 +0100

    Correct meta tag

--- components/Head.js
+++ components/Head.js
@@ -4,11 +4,11 @@ const DEFAULT_HEAD = 'Macoto Ltd - UI Development and Consulting'
 const AppHead = ({ title = DEFAULT_HEAD }) => (
   <Head>
     <title>{title}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="/fonts/fonts.css" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/fonts/fonts.css" />
+    <link key="apple-touch" rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+    <link key="icon32" rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+    <link key="icon64" rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
   </Head>
 );
 
