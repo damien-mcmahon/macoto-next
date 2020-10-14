6f5e9df3cfd5b2c1c6e8a779cefd380a1f1b68b5
Damien McMahon <damien@macoto.co.uk>
2020-10-14 12:12:17 +0100

    Adds Viewport tag

--- components/Head.js
+++ components/Head.js
@@ -4,10 +4,11 @@ const DEFAULT_HEAD = 'Macoto Ltd - UI Development and Consulting'
 const AppHead = ({ title = DEFAULT_HEAD }) => (
   <Head>
     <title>{title}</title>
-    <link rel="stylesheet" href="/fonts/fonts.css" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="/fonts/fonts.css" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
   </Head>
 );
 
