025da9fcda8de6f1cb3babd22848b4cf6145cc56
Damien McMahon <damien@macoto.co.uk>
2020-10-14 12:54:14 +0100

    Adds META description to HEAD

--- components/Head.js
+++ components/Head.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 const DEFAULT_HEAD = 'Macoto Ltd - UI Development and Consulting'
+const DEFAULT_DESCRIPTION = 'Macoto - UI Development, Consulting and Prototype development'
 
-const AppHead = ({ title = DEFAULT_HEAD }) => (
+const AppHead = ({ title = DEFAULT_HEAD, description = DEFAULT_DESCRIPTION }) => (
   <Head>
     <title>{title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,6 +10,7 @@ const AppHead = ({ title = DEFAULT_HEAD }) => (
     <link key="apple-touch" rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
     <link key="icon32" rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
     <link key="icon64" rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+    <meta name="Description" content={description}></meta>
   </Head>
 );
 
