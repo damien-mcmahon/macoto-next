f53d504c87e1b97926b088f4e807328665539f09
Damien McMahon <damien@macoto.co.uk>
2020-10-14 12:59:24 +0100

    Getting font loading actually correct 🤦🏻‍♂️

--- components/Head.js
+++ components/Head.js
@@ -6,7 +6,11 @@ const AppHead = ({ title = DEFAULT_HEAD, description = DEFAULT_DESCRIPTION }) =>
   <Head>
     <title>{title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preload" href="/fonts/fonts.css" as="font" type="font/woff2" crossorigin />
+    <link rel="preload" href="/fonts/jb-mono.woff2" as="font" type="font/woff2" crossorigin />
+    <link rel="preload" href="/fonts/lato-bold.woff2" as="font" type="font/woff2" crossorigin />
+    <link rel="preload" href="/fonts/lato-regular.woff2" as="font" type="font/woff2" crossorigin />
+    <link rel="preload" href="/fonts/lato-semibold.woff2" as="font" type="font/woff2" crossorigin />
+    <link rel="stylesheet" href="/fonts/fonts.css" />
     <link key="apple-touch" rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
     <link key="icon32" rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
     <link key="icon64" rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
