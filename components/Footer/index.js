48b71b5afa80e1db8e0bf6bcd903db960347c30c
Damien McMahon <damien@macoto.co.uk>
2022-01-04 13:32:08 +0000

    Adds dynamic year to save annual updates 😆

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,12 +1,15 @@
 import Container from '../Container';
 import styles from './footer.module.scss';
 
-const Footer = () => (
-  <footer className={styles.wrapper}>
-    <Container>
-      <div className={styles.copyright}>&copy; 2021 - Macoto Ltd</div>
-    </Container>
-  </footer>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className={styles.wrapper}>
+      <Container>
+        <div className={styles.copyright}>&copy; {currentYear} - Macoto Ltd</div>
+      </Container>
+    </footer>
+  );
+}
 
 export default Footer;
